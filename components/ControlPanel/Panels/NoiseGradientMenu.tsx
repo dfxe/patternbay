4ebb3ea61431f1b4d99bc21d1714e51e821f66f7
caravagio <62392399+caravagio@users.noreply.github.com>
2022-05-07 16:59:23 +0100

    Add *

--- components/ControlPanel/Panels/NoiseGradientMenu.tsx
+++ components/ControlPanel/Panels/NoiseGradientMenu.tsx
@@ -94,8 +94,10 @@ const NoiseGradientMenu = () => {
       <svg
         style={{
           position: "absolute",
-          left: "50vw",
+          left: "10vw",
           top: "0vh",
+          width: "100%",
+          height: "100%",
         }}
         viewBox="0 0 100 100"
         xmlns="http://www.w3.org/2000/svg"
